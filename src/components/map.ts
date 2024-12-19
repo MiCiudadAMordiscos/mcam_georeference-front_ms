d79f4dcc6afc0ca7150f785688228e2d3d17febf
brrodriguezd <brrodriguezd@unal.edu.co>
2024-12-18 23:53:47 -0500

    readme

--- src/components/map.ts
+++ src/components/map.ts
@@ -2,7 +2,7 @@ import type { UserPoint } from "@services/fetch_near_restaurants";
 
 // constantes para el mapa
 export const zoom = 17,
-    maxZoom = 20,
+    maxZoom = 19,
     timeout = 10000;
 
 export function createMap(ubicacion: UserPoint) {
